3c2a38d0c68375c5ab7859b204807957ed119ef7
javiperezdev <javiperezdev@gmail.com>
2026-02-11 15:59:43 +0100

    Changed port to be able to host the app in render

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -91,7 +91,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = proccess.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
